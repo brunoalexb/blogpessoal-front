94fa88be6eb27fe73758e7a681e929b54df95ad0
brunoalexb <brunoale77@gmail.com>
2025-01-16 23:28:18 -0300

    router e outras mudanças

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/navbar/footer/Footer'
+import Navbar from './components/Navbar'
+import Home from './pages/home/Home'
+import Cadastro from './pages/home/cadastro/Cadastro'
+import Login from './pages/home/login/Login'
 import './App.css'
-import Home from './pages/home/home'
 
 function App() {
   return (
-    <Home />
-)
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+  )
 }
 
 export default App
