93961fa9db1e14b14b537666e18d61695e5acda0
brunoalexb <brunoale77@gmail.com>
2025-01-23 22:02:27 -0300

    implementando  funcionalidades diversas ao projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,18 @@ import Home from './pages/home/Home'
 import Cadastro from './pages/home/cadastro/Cadastro'
 import Login from './pages/home/login/Login'
 import './App.css'
+import { AuthProvider } from './contexts/AuthContext'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import FormTema from './components/temas/formtema/FormTema'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
 
 function App() {
   return (
     <>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -17,10 +25,19 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
   )
 }
